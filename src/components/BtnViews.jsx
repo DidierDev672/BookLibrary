49e2b6481efca861061937aef31e3708d7990164
DidierDev672 <ariasdev672@gmail.com>
2022-10-18 00:17:25 -0500

    Button to view the sections of the app

---
+++ src/components/BtnViews.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import "../styles/global.scss";
+
+const BtnViews = () => {
+    return(
+        <div className="container py-3">
+            <button type="button" className="btn-views"> Views </button>
+        </div>
+    );
+};
+
+export default BtnViews;
